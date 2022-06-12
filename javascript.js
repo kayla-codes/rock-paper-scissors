711fa7f6010017a7cbb216c374bdecd81dc9fc0c
kayla-codes <hellokayla88@gmail.com>
2022-06-12 16:02:00 -0700

    Modify pseudocode

--- javascript.js
+++ javascript.js
@@ -6,6 +6,19 @@ function computerPlay() {
     return computerChoice;
 }
 
+/* Create a function for player selection called playerPlay
+    Create a loop that:
+        - Prompts for player's selection of rock, paper, scissors and store in variable playerSelection in lowercase
+        - Validate player selection and store in validPlayerSelection
+        - If player selection is valid, break out of loop and return the player selection
+        - If not, then show "Choice is invalid. Try again." and restart loop.
+*/
+
+/* Create a function called validPlayerSelection
+    - playerSelection must match rock, paper, or scissors
+    - Return boolean
+*/
+
 /* Create a function that declares a winner of the round
     - Possible outcomes:
         - rock beats scissors
@@ -15,11 +28,8 @@ function computerPlay() {
     - Returns winner 
 */
 
-// Create a function prompts for player selection
-
 /* Create a function called playRound that plays a single round of Rock Paper Scissors
     - 2 parameters: playerSelection and computerSelection
-        - playerSelection should be case-insensitive
     - Calls computerPlay and stores in computerSelection
     - Calls playerPlay and stores in playerSelection
     - Declare winner and store in roundWinner
