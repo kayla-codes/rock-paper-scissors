64f9b986e7f10b57fc4c261981a77dacdc12204f
kayla-codes <hellokayla88@gmail.com>
2022-06-14 21:09:48 -0700

    Remove pseudocode

--- javascript.js
+++ javascript.js
@@ -1,18 +1,8 @@
-// Create a function called computerPlay to randomly return rock, paper, or scissors
-
 function computerPlay() {
     const gameChoices = ["rock", "paper", "scissors"]
     let computerSelection = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     return computerSelection;
 }
-
-/* Create a function for player selection called playerPlay
-    Create a loop that:
-        - Prompts for player's selection of rock, paper, scissors and store in variable playerSelection in lowercase
-        - Validate player selection and store in validPlayerSelection
-        - If player selection is valid, break out of loop and return the player selection
-        - If not, then show "Choice is invalid. Try again." and restart loop.
-*/
 
 function playerPlay() {
     let playerSelection;
@@ -33,11 +23,6 @@ function playerPlay() {
     }
 }
 
-/* Create a function called playerSelectionValidator
-    - playerSelection must match rock, paper, or scissors
-    - Return boolean
-*/
-
 function validatePlayerSelection(playerSelection) {
     if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
         return true;
@@ -45,15 +30,6 @@ function validatePlayerSelection(playerSelection) {
         return false;
     }
 }
-
-/* Create a function that declares a winner of the round
-    - Possible outcomes:
-        - rock beats scissors
-        - scissors beats paper
-        - paper beats rock
-        - draw
-    - Returns winner 
-*/
 
 function determineRoundWinner(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
@@ -64,14 +40,6 @@ function determineRoundWinner(playerSelection, computerSelection) {
         return "computer"; 
     }
 }
-
-/* Create a function called playRound that plays a single round of Rock Paper Scissors
-    - 2 parameters: playerSelection and computerSelection
-    - Calls computerPlay and stores in computerSelection
-    - Calls playerPlay and stores in playerSelection
-    - Declare winner and store in roundWinner
-    - returns a string that declares the winner    
-*/
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerPlay();
@@ -86,8 +54,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Create a function to announce round winner 
-
 function announceRoundWinner(roundWinner, playerSelection, computerSelection) {
     if (roundWinner === "player") {
         console.log(`You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`);
@@ -101,9 +67,6 @@ function announceRoundWinner(roundWinner, playerSelection, computerSelection) {
     }
 }
 
-
-// Create a function that declares winner of the round
-
 function announceGameWinner(playerScore, computerScore) {
     if (playerScore > computerScore) {
         return "Congrats! You win!";
@@ -113,13 +76,6 @@ function announceGameWinner(playerScore, computerScore) {
         return "No winner. How dull.";
     }
 }
-
-/* Create a function called game 
-    - Calls the playRound function
-    - Plays 5 rounds
-    - Keeps score 
-    - Reports a winner
-*/
 
 function game() {
     let playerScore = 0;
